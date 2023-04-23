138b6097c49ad9f4682f0936fea005d380726575
teilomillet <teilomillet@gmail.com>
2023-04-23 14:08:00 +0200

    add basics types

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Here we are ...');
+function add(n1, n2, showResult, phrase) {
+    var result = (n1 + n2);
+    if (showResult = true) {
+        console.log(phrasing + result);
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 3;
+var printResult = true;
+var phrasing = 'Result is: ';
+add(number1, number2, printResult, phrasing);
